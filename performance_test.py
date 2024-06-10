35a0e0421ff726002912c603c48a55d4dd7a17a7
Aleksei Zuev <aleksei.zuev@tuni.fi>
2024-06-10 12:35:42 +0300

    py tester v1.0

--- performance_test.py
+++ performance_test.py
@@ -1,12 +1,29 @@
 import subprocess
-import iperf3
+import os
 
 def test_iperf3():
     try:
         subprocess.run("iperf3",shell=True,text=True)
+        return True
     except subprocess.CalledProcessError:
         print("Oh, you need to install iperf3")
+        return False
+
+def server_run(NS1, interface1):
+    args = ['ip','netns','exec',NS1,'iperf3','-s', interface1]
+    print(args)
+    s = subprocess.Popen(args)
+    return s, True
 
 
-def server(NS1, interface1):
-    
+
+def client_run(NS2, interfaces):
+    args = ['ip','netns','exec',NS2,'iperf3','-c',interfaces[0][1],'-B',interfaces[1][1],'-b 0']
+    print(args)
+    c = subprocess.Popen(args)
+    return c
+
+def iperf_kill(iperf):
+    iperf.kill()
+    print('Server is dead')
+
--- run_test.py
+++ run_test.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
+
 from performance_test import *
+import sys
 
 
-
+def add_dummy_ifaces():
+    subprocess.run('ip l a if1 type dummy', shell=True, text=True)
+    subprocess.run('ip l a if2 type dummy', shell=True, text=True)
 def add_vpn_interface(namespaces, interfaces):
-    for x in range(1):
-        subprocess.run(f"ip l set {interfaces[x]} netns {namespaces[x]}",shell=True, text=True)
+    for x in range(2):
+        subprocess.run(f"ip l set {interfaces[x][0]} netns {namespaces[x]}",shell=True, text=True)
+        subprocess.run(f"ip -n {namespaces[x]} a a {interfaces[x][-1]}/24 dev {interfaces[x][0]}",shell=True, text=True)
+        subprocess.run(f"ip -n {namespaces[x]} l set {interfaces[x][0]} up ", shell=True, text=True)
+        addr = interfaces[x][-1]
+        subnet = addr.split('.')[:-1]
+        subnet = '.'.join(subnet) + '.0'
+        subprocess.run(f"ip -n {namespaces[x]} r d {subnet}/24", shell=True, text=True)
+    subprocess.run("ip -n s1 r a default via 1.1.1.2 dev veth1", shell=True, text=True)
+    subprocess.run("ip -n s2 r a default via 1.1.1.1 dev veth2", shell=True, text=True)
 
 def add_bridges(namespaces):
     for NS in namespaces:
-        subprocess.run(f'ip netns exec {NS[0]} ip l a br0 type bridge ', shell=True, text=True)
-        subprocess.run(f'ip netns exec {NS[0]} ip l set br0 up', shell=True, text=True)
+        subprocess.run(f'ip netns exec {NS} ip l a br0 type bridge ', shell=True, text=True)
+        subprocess.run(f'ip netns exec {NS} ip l set br0 up', shell=True, text=True)
 
 def  CleanAll(NS1, NS2):
-    subprocess.run(f'ip netns d  {NS1[0]}', shell=True, text=True)
-    subprocess.run(f'ip netns d {NS2[0]}', shell=True, text=True)
+    subprocess.run(f'ip netns d  {NS1}', shell=True, text=True)
+    subprocess.run(f'ip netns d {NS2}', shell=True, text=True)
     res = subprocess.run('ip netns list',shell=True, text=True, capture_output=True)
     print(f"I'm careful, \nso I've deleted all I've created before, here's 'ip netns list' output", res.stdout.splitlines())
 
@@ -23,11 +35,13 @@ def  CleanAll(NS1, NS2):
 def CreateNameSpaces(namespaces):
     subprocess.run(f'ip link add veth1 type veth peer name veth2', shell=True, text=True)
     for NS in namespaces:
-        subprocess.run(f'ip netns add  {NS[0]}', shell=True, text=True)
-        subprocess.run(f'ip link set veth{NS[0][-1]} netns {NS[0]}', shell=True, text=True)
+        subprocess.run(f'ip netns add  {NS}', shell=True, text=True)
+        subprocess.run(f'ip link set veth{NS[-1]} netns {NS}', shell=True, text=True)
+        subprocess.run(f'ip -n {NS} a a 1.1.1.{NS[-1]}/24 dev veth{NS[-1]}', shell=True, text=True)
+        subprocess.run(f'ip -n {NS} l set veth{NS[-1]} up', shell=True, text=True)
     res = subprocess.run('ip netns list',shell=True, text=True, capture_output=True)
-    print(f'NAME SPACES ARE CREATED \n',res.stdout.splitlines(),f'\nveth0 INTERFACE FOR NAMESPACE{namespaces[0][0]} '
-          f'\nveth1 INTERFACE FOR NAMESPACE {namespaces[0][0]}\n')
+    print(f'NAME SPACES ARE CREATED \n',res.stdout.splitlines(),f'\nveth1 INTERFACE FOR NAMESPACE {namespaces[0]} '
+          f'\nveth2 INTERFACE FOR NAMESPACE {namespaces[1]}\n')
 
 
 import subprocess
@@ -37,20 +51,27 @@ def main():
     if subprocess.run('whoami',shell=True, text=True, capture_output=True).stdout.find('root'):
         print(subprocess.run('whoami',shell=True, text=True, capture_output=True).stdout)
         exit("Should run as root user, restart ")
-    NS1 = ['s1','1.1.1.1']
-    interface1 = 'if1'
-    NS2 = ['s2','2.2.2.2']
-    interface2 = 'if2'
+    if len(sys.argv) != 2 :
+        print('You need only two arguments, they are VPN interface names')
+    vpn2 = sys.argv[2]
+    vpn1 = sys.argv[1]
+    NS1 = 's1'
+    interface1 = [vpn1, '12.23.34.45']
+    NS2 = 's2'
+    interface2 = [vpn2, '23.34.45.56']
     interfaces = [interface1, interface2]
     namespaces = [NS1,NS2]
     CreateNameSpaces(namespaces)
-    add_bridges(namespaces)
+    #add_dummy_ifaces()
+
+
+    #add_bridges(namespaces)
     add_vpn_interface(namespaces, interfaces)
-
-
-
-
-
+    server, running_server=server_run(NS1, interface1[1])
+    if running_server:
+        client = client_run(NS2, interfaces)
+        client.wait()
+    iperf_kill(server)
     CleanAll(NS1,NS2)
 
 
