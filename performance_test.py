d434555c0e83ceb33ed2511cad678cb7ec22a337
Alex Pyattaev <me@example.com>
2024-06-23 23:03:39 +0300

    less shitty tests

--- performance_test.py
+++ performance_test.py
@@ -1,23 +1,22 @@
 import subprocess
 
-def test_iperf3():
+def check_has_iperf3():
     try:
         subprocess.run("iperf3",shell=True,text=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
         return True
     except subprocess.CalledProcessError:
-        print("Oh, you need to install iperf3")
         return False
 
-def server_run(NS:str, server_ip:str):
-    args = ['ip','netns','exec',NS,'iperf3','-s','-D', server_ip]
+def iperf_server_run(NS:str, server_ip:str):
+    args = ['ip','netns','exec',NS,'iperf3','-s','--one-off','-D', server_ip]
     print(" ".join(str(element) for element in args))
     s = subprocess.Popen(args)
-    return s, True
+    return s
 
 
 
-def client_run(NS:str, serv_ip:str, client_ip:str):
-    args = ['ip','netns','exec',NS,'iperf3','-c',serv_ip,'-B',client_ip,'-b 0','-i 0.125','--bidir']
+def iperf_client_run(NS:str, serv_ip:str, client_ip:str):
+    args = ['ip','netns','exec',NS,'iperf3','-c',serv_ip,'-B','--time','3',client_ip,'-b 0','-i 0.125','--bidir']
     print(" ".join(str(element) for element in args))
     c = subprocess.Popen(args)
     return c
@@ -26,3 +25,10 @@ def iperf_kill(iperf):
     iperf.kill()
     print('Server R.I.P.')
 
+def ping_test_client_run(NS:str, source_ip:str, target_ip:str, packet_size:int=1500, count:int=10, flood=False):
+    args = ['ip','netns','exec',NS,'ping','-I',source_ip, '-c',str(count), '-s', str(packet_size), target_ip]
+    if flood:
+        args.append('-f')
+    print(" ".join(str(element) for element in args))
+    c = subprocess.Popen(args)
+    return c
--- run_test.py
+++ run_test.py
@@ -3,8 +3,12 @@
 import os
 import sys
 import subprocess
-from performance_test import server_run, client_run, iperf_kill
+from typing_extensions import Callable
+from performance_test import iperf_server_run, iperf_client_run, iperf_kill, check_has_iperf3, ping_test_client_run
 from time import sleep
+from contextlib import contextmanager
+from typing import Iterable, Iterator, Tuple
+from traceback import print_exc
 
 def run_cmd(args:str):
     print(f"# {args}")
@@ -23,7 +27,6 @@ class Namespace:
         self.ip_addr = ip_addr
         self.port = port
         self.remote = remote
-        self.print_logs = False
         self.test_ip = test_ip
 
 
@@ -38,20 +41,23 @@ class Namespace:
         run_cmd(f"ip -n {self.name} a a {self.ip_addr}/24 dev {self.veth_name}")
         run_cmd(f"ip -n {self.name} l set {self.veth_name} up ")
 
-    def spawn_vpn_perf(self):
-        self.spawn_vpn(log_level="critical")
+    def spawn_vpn_debug(self):
+        self.spawn_vpn(log_level="debug", print_logs=True)
 
-    def spawn_vpn(self, log_level="debug"):
+    def spawn_vpn_perf(self):
+        self.spawn_vpn(log_level="critical", print_logs=False)
+
+    def spawn_vpn(self, log_level:str, print_logs:bool):
         cmd = (f'ip netns exec {self.name} ./target/release/vpn -r {self.remote}:{self.port} -l {self.ip_addr}:{self.port} ')
         print(f'# {cmd}')
         #inherit env
         new_env = os.environ.copy()
         #enable full tracing from VPN impl
         new_env.update({"TRACE_LOG":log_level })
-        l = sys.stdout if self.print_logs  else None
+        err = sys.stdout if print_logs  else subprocess.DEVNULL
+        out = sys.stdout if print_logs  else subprocess.DEVNULL
 
-
-        self.vpn = subprocess.Popen(cmd.split(), env=None, stdout=subprocess.DEVNULL, stderr=l)
+        self.vpn = subprocess.Popen(cmd.split(), env=new_env, stdout=out, stderr=err)
         #subnet = self.ip_addr.split('.')[:-1]
         #subnet = '.'.join(subnet) + '.0'
         #run_cmd(f"ip -n {self.namespace} r d {subnet}/24")
@@ -71,7 +77,7 @@ class Namespace:
 
 
 
-def add_bridge(name:str, veths:list[str]):
+def add_intra_namespace_bridge(name:str, veths:list[str]):
     up = subprocess.run(f'ip l set {name} up', shell=True, text=True,
                         stderr=subprocess.DEVNULL,stdout=subprocess.DEVNULL)
 
@@ -90,91 +96,157 @@ def set_default_routes(NS1:Namespace, NS2:Namespace):
 
 
 
+def test(func:Callable):
+    from functools import wraps
 
-def  CleanAll(namespaces:list[Namespace]):
-    #run_cmd(f"killall wireshark")
-    run_cmd(f"killall iperf3")
+    @wraps(func)
+    def __run(*args, **kwargs):
+        print("="*20)
+        print("="*20)
+        print(f"Test starting {func.__name__}({','.join(args)}, {kwargs})")
+        try:
+            func(*args, **kwargs)
+        except Exception as e:
+            print(f"Test {func.__name__}({','.join(args)}, {kwargs}) failed with exception {e}")
+            print_exc()
+        else:
+            print(f"Test {func.__name__}({','.join(args)}, {kwargs}) successful")
+        print("="*20)
+        print("="*20)
+    return __run
 
-    run_cmd(f"ip l d monitor_bridge")
+
+@test
+def manual_test():
+    with namespace_setup() as  namespaces:
+        with start_VPN(namespaces) as vpns:
+            input("Setup complete, press enter to tear stuff down")
 
 
+@test
+def perf_test_tcp():
+    with namespace_setup() as  namespaces:
+        with start_VPN(namespaces) as vpns:
+            ns1, ns2 = namespaces
+            server = iperf_server_run(NS=ns1.name, server_ip=ns1.test_ip)
+            try:
+                sleep(0.1)
+                client = iperf_client_run(NS=ns2.name, client_ip=ns2.test_ip, serv_ip=ns1.test_ip)
+                client.wait()
+            finally:
+                iperf_kill(server)
+
+@test
+def correctness_small_packets():
+    with namespace_setup() as  namespaces:
+        with start_VPN(namespaces) as vpns:
+            ns1, ns2 = namespaces
+            c = ping_test_client_run(NS=ns1.name,  source_ip=ns1.test_ip, target_ip=ns2.test_ip, packet_size=150)
+            rc = c.wait()
+            if rc !=0:
+                raise RuntimeError("Test failed")
+
+@test
+def correctness_big_packets():
+    with namespace_setup() as  namespaces:
+        with start_VPN(namespaces) as vpns:
+            ns1, ns2 = namespaces
+            c = ping_test_client_run(NS=ns1.name,  source_ip=ns1.test_ip, target_ip=ns2.test_ip, packet_size=1500)
+            rc = c.wait()
+            if rc !=0:
+                raise RuntimeError("Test failed")
+
+@contextmanager
+def namespace_setup(NS1:str = 's1',    NS2:str = 's2',mon_br_name:str = 'monitor_bridge')->Iterator[Tuple[Namespace, Namespace]]:
+    namespaces:tuple=()
+    try:
+        # Create the actual namespaces
+        interface1 = Namespace(name= NS1, ip_addr='1.1.1.3', port=7777, remote='1.1.1.4',test_ip='2.2.2.1')
+        interface2 = Namespace(name= NS2, ip_addr='1.1.1.4', port=7777, remote='1.1.1.3',test_ip='2.2.2.2')
+        namespaces = (interface1, interface2)
+
+        # Create the logical ethernet interfaces to the outside world
+        for n in namespaces:
+            n.spawn_veths()
+
+        # Create a bridge between namespaces for monitoring (i.e. "internet")
+        add_intra_namespace_bridge(mon_br_name, [i.outer_veth_name for i in namespaces])
+        sleep(0.1)
+
+        # configure the interfaces between namespaces to be able to send packets via "internet"
+        for n in namespaces:
+            n.ip_config()
+
+        # set mutual routes between namespaces
+        set_default_routes(interface1, interface2)
+
+        # add bridge ports with individual IPs within namespaces for traffic that will go via VPN
+        for n in namespaces:
+            n.test_bridge()
+
+        yield namespaces
+
+
+    finally:
+
+        run_cmd(f"killall iperf3")
+        run_cmd(f"ip l d {mon_br_name}")
+
+        for n in namespaces:
+            run_cmd(f"ip l d {n.outer_veth_name}")
+            run_cmd(f'ip netns d  {n.name}')
+
+        res = subprocess.run('ip netns list',shell=True, text=True, capture_output=True)
+        print(f"I've deleted all namespaces I've created before, here's 'ip netns list' output", res.stdout.splitlines())
+
+
+@contextmanager
+def start_VPN(namespaces:Tuple[Namespace, Namespace], profiles:Tuple[str,str]=("debug", "perf")):
+    for n, p in zip(namespaces, profiles):
+        if p =="debug":
+            n.spawn_vpn_debug()
+        elif p=="perf":
+            n.spawn_vpn_perf()
+        else:
+            raise NotImplementedError(f"Profile {p} not implemented")
+
+    yield
+
+    # signal the VPN servers to stop
+    import signal
     for n in namespaces:
-        run_cmd(f"ip l d {n.outer_veth_name}")
-        run_cmd(f'ip netns d  {n.name}')
+        n.vpn.send_signal(sig=signal.SIGINT)
+        sleep(0.5)
+        n.vpn.poll()
+    sleep(0.5)
 
-    res = subprocess.run('ip netns list',shell=True, text=True, capture_output=True)
-    print(f"I'm careful, \nso I've deleted all I've created before, here's 'ip netns list' output", res.stdout.splitlines())
-
-
-
-
-
+    # kill the VPN servers
+    for n in namespaces:
+        n.vpn.poll()
+        n.vpn.terminate()
+        n.vpn.kill()
 
 
 def main():
-
-
     if not os.path.exists('./target/release/vpn'):
         exit("There should be a built release binary to test, run ./build.sh as a user to make one")
+
+    if not check_has_iperf3():
+        exit("IPerf3 is not detected/functional")
 
     if os.geteuid() != 0:
         exit("Should run as root user, else namespaces can not be created")
 
-    NS1 = 's1'
-    NS2 = 's2'
-    mon_br_name = 'monitor_bridge'
-    namespaces=[]
-    try:
-        interface1 = Namespace(name= NS1, ip_addr='1.1.1.3', port=7777, remote='1.1.1.4',test_ip='2.2.2.1')
-        interface2 = Namespace(name= NS2, ip_addr='1.1.1.4', port=7777, remote='1.1.1.3',test_ip='2.2.2.2')
-        namespaces = [interface1, interface2]
 
-        for n in namespaces:
-            n.spawn_veths()
+    #run_cmd("ip -n s1 r a default via 1.1.1.2 dev veth1")
+    #run_cmd("ip -n s2 r a default via 1.1.1.1 dev veth2")
+    #subprocess.Popen(['wireshark','-i',mon_br_name,'-k'])
 
-        add_bridge(mon_br_name, [i.outer_veth_name for i in namespaces])
-        sleep(0.5)
-        #run_cmd("ip -n s1 r a default via 1.1.1.2 dev veth1")
-        #run_cmd("ip -n s2 r a default via 1.1.1.1 dev veth2")
-        #subprocess.Popen(['wireshark','-i',mon_br_name,'-k'])
-        sleep(1.5)
+    correctness_small_packets()
+    correctness_big_packets()
 
-
-
-
-        for n in namespaces:
-            n.ip_config()
-
-        #namespaces[0].print_logs = True
-
-        set_default_routes(interface1, interface2)
-
-        for n in namespaces:
-            n.spawn_vpn_perf()
-
-        for n in namespaces:
-            n.test_bridge()
-
-
-
-        input("Press enter to run perf test")
-
-        server, running_server=server_run(NS=NS1, server_ip=interface1.test_ip)
-        try:
-            sleep(0.1)
-            if running_server:
-                client = client_run(NS=NS2, client_ip=interface2.test_ip,serv_ip=interface1.test_ip)
-                client.wait()
-        finally:
-            iperf_kill(server)
-
-        for n in namespaces:
-            n.vpn.kill()
-
-
-    finally:
-        CleanAll(namespaces)
-
+    #perf_test_tcp()
+    #manual_test()
 
 
 
